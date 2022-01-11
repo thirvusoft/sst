211d152e9b1f2b97adec972e766b239b96190b17
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-01-11 09:55:13 +0000

    item_filter

--- shiva_sakkthi_printers/item.js
+++ shiva_sakkthi_printers/item.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on("Item", {
 	setup: function(frm) {
 		frm.set_query("brand", function() {
-		    console.log('Item');
 		    let customer_name=frm.doc.customer_name;
 			return {
 				filters: [
