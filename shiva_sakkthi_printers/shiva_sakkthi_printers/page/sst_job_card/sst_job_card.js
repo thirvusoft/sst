3ea12b72a1755715053623aa964290267e3506dc
hiraniya <hiraniya@thirvusoft.in>
2022-02-28 16:02:58 +0530

    jc

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.js
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.js
@@ -7,12 +7,10 @@ frappe.pages['sst-job-card'].on_page_load = function(wrapper) {
     
 
     page.set_indicator('Manufacturing order', 'green')
-    let $btn = page.set_primary_action('Start', () =>open_work_order(frm), true )
-    //page.add_menu_item('Send Email', () => open_email_dialog())
-    //page.add_inner_button('New Post', () => new_post(), 'Work Order')
+    let $btn = page.set_primary_action('Start',()=>open_work_order())
+    
 
-   // let $btn = page.set_secondary_action('Refresh', () => refresh(), 'octicon octicon-sync')
-   let field = page.add_field({
+    let field = page.add_field({
         label: 'Tag',
         fieldtype: 'Select',
         fieldname: 'status',
@@ -25,48 +23,11 @@ frappe.pages['sst-job-card'].on_page_load = function(wrapper) {
             console.log(field.get_value());
         }
     });
-      //  field = page.add_field({
-      //  label: 'Customer',
-      //  fieldtype: 'Link',
-      //  fieldname: 'customer',
-      //  options: 'Sales Order',
-      //  change() {
-       //     console.log(field.get_value());
-      //  }
-   // });
-  function open_work_order(frm) {
-        frappe.new_doc('Work Order', {
-           work_order: frm.doc.name
-            })
+    function open_work_order() {
+        frappe.set_route("work-order");
     }
+
      
 }
 
-
-
- // page.start = 0;
-
- // page.warehouse_field = page.add_field({
-   //       fieldname: 'warehouse',
-     //     label: __('Warehouse'),
-       //   fieldtype:'Link',
-         // options:'Warehouse',
-          //change: function() {
-            //      page.item_dashboard.start = 0;
-              //    page.item_dashboard.refresh();
-        //  }
-  //});
-
-  //page.customer_field= page.add_filed({
-    //      fieldname:"customer",
-      //    label:__('Customer'),
-        //  fieldtype:'Link',
-          //options:'Customer',
-         // change: function() {
-           //       page.sales_order_dashboard.start = 0;
-             //     page._dashboard.refresh();
-          //}
-
-  //});
-//}
 
