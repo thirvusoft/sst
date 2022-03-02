68d2e29cc818f761b3b1857010ccccbc8e267cd2
hiraniya <hiraniya@thirvusoft.in>
2022-03-02 10:46:57 +0530

    Job Card Page

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.js
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.js
@@ -5,91 +5,82 @@ frappe.pages['sst-job-card'].on_page_load = function(wrapper) {
         single_column: true
     });
     
-var a="\nSAL-ORD-2022-00027\nSAL-ORD-2022-00016";
+//var a="\nSAL-ORD-2022-00027\nSAL-ORD-2022-00016";
     page.set_indicator('Manufacturing order', 'green')
     page.set_primary_action('Start', () =>open_work_order(), true )
-    x = new frappe.ui.FieldGroup({
-    fields: [
+      this.form = new frappe.ui.FieldGroup({
+      fields: [
       {
         label: 'Tag',
         fieldtype: 'Link',
         fieldname: 'status',
         options:"Tag",
-        change : () => frappe.call({
-            method:"shiva_sakkthi_printers.shiva_sakkthi_printers.page.sst_job_card.sst_job_card.sofilter",
-            args:{
-              "status":x.get_value("status")
-            },
-            callback:function(r){
-              a=r.message;
-              console.log(a);
-              x.get_field('salesorder').set_value(a);  
-            }
-          })
+      
       },
 
       {
       fieldtype: 'Column Break'
-<<<<<<< HEAD
-    },
-
-
-  {
-    label: 'Sales Order',
-    fieldtype: 'Link',
-    fieldname: 'salesorder',
-    options: 'Sales Order',
-    "get_query":function() {
-      return {
-        filters :{ "_user_tags":",HIGH"}
-          
-      }
-    },
-
-    },
-    {
-=======
       },
-      {
+      
+
+
+     {
       label: 'Sales Order',
-      fieldtype: "Select",
+      fieldtype: 'Link',
       fieldname: 'salesorder',
-      options:a,
-      change: () => frappe.call({
-        method: "shiva_sakkthi_printers.shiva_sakkthi_printers.page.sst_job_card.sst_job_card.workorder",
-        args:{
-          so: x.get_value("salesorder")
-        },
-        callback: function(r) {
-          console.log(r.message)
-          page.clear_fields();
-          for(let i=0;i<r.message.length;i++){
-          page.add_field({
-            label: 'Customer Name',
-            fieldtype: 'Read Only',
-            fieldname: 'customer_name',
-            default: r.message[i].customer
-         
-          })
-          page.add_field({
-            label: 'Work Order',
-            fieldtype: 'Read Only',
-            fieldname: 'workorder',
-            default: r.message[i].name
-         
-          })
-        page.add_field({
-          fieldtype:"HTML",
-          options:"<hr>"
+      options: 'Sales Order',
+     // "get_query":function() {
+      //  return {
+       //   filters :{ "_user_tags":",HIGH"}
+          
+       // }
+      //}
+      onchange:function(){
+        if(this.value){
+          console.log(this)
+          frappe.call({
+            method: "shiva_sakkthi_printers.shiva_sakkthi_printers.page.sst_job_card.sst_job_card.workorder",
+            args:{
+              salesorder: this.value
+            },
+            callback:function(r){
+              console.log(r["message"])
+              this.customer_name.set_value(r["message"]);
+              this.customer_name.refresh()
+          
+              console.log("setted")
+            }
         })
-        } }
-      })
-      },
-      {
->>>>>>> 4b5f40771456a6c2f65bbb7692ff20a9884c0144
+
+        }
+
+      }
+     },
+     // function filter() {
+     //   var keyword = document.getElementById("search").value;
+     //   var select = document.getElementById("select");
+       // for (var i = 0; i < select.length; i++) {
+       //     var txt = select.options[i].text;
+       //     if (!txt.match(keyword)) {
+            //    $(select.options[i]).attr('disabled', 'disabled').hide();
+        //    } else {
+        //        $(select.options[i]).removeAttr('disabled').show();
+         //   }
+
+       // }
+    
+     
+     {
       fieldtype: 'Section Break'
-      },
-      {
+     },
+     {
+      label: 'Customer Name',
+      fieldtype: 'Data',
+      fieldname: 'customer_name',
+    },
+
+
+      /*{
       fetch_from: "salesorder.customer",
       label: 'Customer Name',
       fieldtype: 'Read Only',
@@ -97,31 +88,29 @@ var a="\nSAL-ORD-2022-00027\nSAL-ORD-2022-00016";
       },
       {
       fieldtype: 'Section Break'
-      },
-      {
+      },*/
+     {
       fieldtype:"HTML",
       fieldname:"preview",
-      options:"bbb"
-      }
+    
+     }
+    
 
      ],
-      body: this.page.body
-    } );
+     body: this.page.body
+     } );
+    
+     this.form.make();
+    
    
-   x.make();
-   
-   
+   //make_form()
   function open_work_order() {
     frappe.set_route("work-order");
     }  
-<<<<<<< HEAD
-/*
+
   
-  frappe.call({
-      method: "shiva_sakkthi_printers.shiva_sakkthi_printers.page.sst_job_card.sst_job_card.workorder",
-      args:{
-        so: "SAL-ORD-2022-00027"
-      },
+  
+      /*
       callback: function(r) {
         console.log(r.message)
         for(let i=0;i<r.message.length;i++){
@@ -141,13 +130,52 @@ var a="\nSAL-ORD-2022-00027\nSAL-ORD-2022-00016";
         
       } }
     });
-    for(let i=0;i<3;i++){
+    for(let i=0;i<3;i++){*/
     this.form.get_field('preview').html(`
-   aaa
+    <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+    table, th, td {
+      border:1px solid black;
+      border-collapse: collapse;
+    }
+      
+    </style>
+    <div id="orange" style="position:relative">
+    <table>
+      <tr>
+        <th><center>S.No</center></th>
+        <th><center>Design Name/Size</center></th>
+        <th><center>Qantity</center></th>
+        <th><center>Stock</center></th>
+        <th><center>Production</center></th>
+        <th><center>No Of Paper</center></th>
+      </tr>
+      <% int i = 1; %>
+      
+        <c:forEach>
+      <tr>
+        <td><center>{{<%= i %> <% i++; %>}}</center></td>
+        <td>{{}}</td>
+        <td>{{}}}</td>
+        <td>{{}}}</td>
+        <td>{{}}}</td>
+        <td>{{}}}</td>
+      </tr>
+      </c:if>
+    </table>
+    </div>
+    
 		`);
-    }*/
-=======
->>>>>>> 4b5f40771456a6c2f65bbb7692ff20a9884c0144
+    
 }
 
 
