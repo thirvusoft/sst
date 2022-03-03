e657f45ce777e2f74c3980518409f30b0e29459c
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-03-03 15:50:25 +0530

    Custom Job card fields with filters

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.py
@@ -1,27 +1,39 @@
 import frappe
-@frappe.whitelist(allow_guest=True)
-def workorder(salesorder):
-  #  a= frappe.get_all("Work Order",filters={"sales_order":so})
-   b=frappe.get_all("Sales Order",fields=['name','customer'])
-   for i in range(0,len(b),1) :
-       if salesorder == b[i]['name']:
-          customer=b[i]['customer']
-          return(customer)
-    #x=[]
-    #for i in a:
-     #   i.customer=b
-      #  x.append(i)
-    #return x
-#@frappe.whitelist()
-#def sofilter(status):
- #   if(status==""):
-  #      salesorder=[i.name for i in frappe.get_all("Sales Order")]
-   # else:
-    #    so=frappe.get_all("Sales Order")
-     #   salesorder=[]
-      #  for i in so:
-       #     if(frappe.get_value("Sales Order",i.name,"_user_tags")!=None):
-        #        if(status in frappe.get_value("Sales Order",i.name,"_user_tags")):
-         #          salesorder.append(i.name)
-    #ret="\\n".join(salesorder)
-    #return ret
+@frappe.whitelist()
+def workorder(so):
+    a= frappe.get_all("Work Order",filters={"sales_order":so})
+    b=frappe.get_value("Sales Order","SAL-ORD-2022-00027","customer")
+    x=[]
+    for i in a:
+        i.customer=b
+        x.append(i)
+    return x
+    
+    
+    
+@frappe.whitelist()
+def sofilter(status='',customer=''):
+    filters={'status':['!=',"Completed"]}
+    if(customer!=""):
+      filters['customer']=customer
+    so=salesorder=[i.name for i in frappe.get_all("Sales Order",filters)]
+    if(status!=''):
+      salesorder=[]
+      for sales in so:
+        tag=frappe.get_value("Sales Order",sales,"_user_tags")
+        print(tag,sales)
+        if(tag!=None):
+          if(status in tag.split(',')):
+            salesorder.append(sales)
+    return ['']+salesorder
+    
+    
+    
+@frappe.whitelist()
+def setup():
+  filters={'status':['!=',"Completed"]}
+  return { 
+  "tags":['']+[i.name for i in frappe.get_all("Tag")],
+  "salesorder":['']+[i.name for i in frappe.get_all("Sales Order",filters=filters)],
+  "customer":list(set(['']+[ i.customer for i in frappe.get_all("Sales Order",filters=filters,fields="customer")]))
+  }
