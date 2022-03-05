cea14fde33b09603cd97e18296bc9c5da6dc4678
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-03-05 13:05:30 +0530

    Custom job card table added button added but not completed

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_job_card/sst_job_card.py
@@ -1,4 +1,6 @@
 import frappe
+from erpnext.stock.dashboard.item_dashboard import get_data
+
 @frappe.whitelist()
 def workorder(so):
     a= frappe.get_all("Work Order",filters={"sales_order":so})
@@ -42,11 +44,30 @@ def setup():
 @frappe.whitelist()
 def jobcarddetails(salesorder):
   if(salesorder!=''):
-    work_order=[frappe.get_doc("Work Order",i) for i in frappe.get_all("Work Order",filters={'sales_order':salesorder,"status":['!=','Cancelled']})]
-    work_order_dict=[{'workorder':i.item_name,'qty':int(i.qty),'stock':0 ,'produced_qty':int(i.produced_qty), 
-      'in_process':int(i.material_transferred_for_manufacturing-i.produced_qty),'not_started':int(i.qty-i.material_transferred_for_manufacturing),'raw_material':i.required_items[0].item_name,'raw_material_qty':int(i.required_items[0].required_qty)}
-      for i in work_order]
-    return jobcardhtml(work_order_dict) if(len(work_order_dict)>0) else '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"></head>'
+    work_order=[frappe.get_doc("Work Order",i) for i in frappe.get_all("Work Order",filters={'sales_order':salesorder,"status":['!=','Cancelled'],"docstatus":1})]
+    work_order_dict=[{
+      'workorder':i.item_name,
+      'qty':int(i.qty),
+      'stock':0 if(get_data(item_code=frappe.get_all("Item",{"item_name":i.item_name})[0].name , warehouse="Finished Goods - SST")==[]) else int(get_data(item_code=frappe.get_all("Item",{"item_name":i.item_name})[0].name , warehouse="Finished Goods - SST")[0]['actual_qty']) ,
+      'produced_qty':int(i.produced_qty), 
+      'in_process':int(i.material_transferred_for_manufacturing-i.produced_qty),
+      'not_started':int(i.qty-i.material_transferred_for_manufacturing),
+      'raw_material':i.required_items[0].item_name,
+      'raw_material_qty':int(i.required_items[0].required_qty)  }  for i in work_order]
+    status=[ i.status for i in work_order]
+    return jobcardhtml(work_order_dict,status,salesorder) if(len(work_order_dict)>0) else '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"></head> <center><b> NO JOB CARDS </b></center>'
+ 
+ 
+def statusindicator(status):
+  color={
+    "In Process": "orange",
+    "Completed": "green",
+    "Not Started": "red",
+    "Stopped": "red",
+    "Closed": "black",
+    "Cancelled": "red"
+  }
+  return f'<span class="indicator-pill whitespace-nowrap {color[status]}"><span style="font-weight:bold;font-size:14px;">{status}</span></span>'
 
 
 def progress(produced,inprocess,not_started):
@@ -65,21 +86,78 @@ def progress(produced,inprocess,not_started):
   '''
   return bar
 
-def jobcardhtml(wo):
+def button():
+  return '''
+    <button class="start" type="button" style="background-color:#4da6ff; margin-bottom:4px;"> Start </button>
+    <button class="finish" type="button" style="background-color:#99ffcc;"> Finish </button>
+  '''
+
+
+def jobcardinfohtml(salesorder):
+  so=frappe.get_doc('Sales Order',salesorder)
+  infohtml=f'''
+    <div class='div'>
+      <div class='jobcardinfo'>
+        Customer Name : {so.customer}<br>
+        Delivery Date : {so.delivery_date}<br><br>
+      </div>
+      <div class='jobcardinfo'>
+        Job Card NO : {so.name}<br>
+        PO No : {so.po_no}<br><br>
+      </div>
+    </div>
+  '''
+  return infohtml
+
+
+def jobcardhtml(wo,status,salesorder):
+  print('\n'*5,wo,'\n'*5)
   head='<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"></head>'
   style='''<style> 
-              tr,th, td{
-                border-bottom: 1px solid black;
-                border-left:1px solid grey;
+              tr,th,td {
+                border-bottom: 1px solid white;
+                border-left:1px solid white;
                 height:50px;
                 font-size:15px;
                 text-align:center;
+                border-radius:10px 15px;
+                border-collapse: collapse
               } 
+              th{
+                background-color:#ff9933;
+                font-size:17px;
+              }
               table{
+                font-weight:bold;
                 width:100%;
-                border:1px solid black;
+              }
+              button{
+                border-radius:10px;
+                font-weight:bold;
+                padding:5px;
+              }
+              .jobcardinfo{
+                float:left;
+                width:50%;
+                font-size:17px;
+                line-height:2;
+              }
+              .div{
+              background-color:#33307c;
+              color:white;
+              font-weight:bold;
+              border-radius:10px;
+              height:100px;
+              width:100%;
+              margin-bottom:5px;
+              padding:20px;
               }
           </style>'''
-  html='<tr>'+''.join([f'<th>{i}</th>' for i in ['S.No','Item Name','Quantity','Stock','Produced Quantity','Production in process','Production Not Started','Paper','No of Paper']])+'</tr>'
-  td=['<tr>'+f'<td>{wo_details+1}</td>'+''.join([f'<td>{wo[wo_details][list(wo[wo_details].keys())[i]]}</td>' for i in range(len(wo[wo_details]))])+f'</tr><tr><td colspan=9>{progress(wo[wo_details]["produced_qty"],wo[wo_details]["in_process"],wo[wo_details]["not_started"])}</td></tr>' for wo_details in range(len(wo))]
-  return head+style+'<table>'+html+''.join(td)+'</table>'
+  html='<tr>'+''.join([ f'<th>{i}</th>' for i in ['S.No','Item Name','Quantity','Stock','Produced Quantity','Production in process','Production Not Started','Paper','No of Paper','Status','Actions'] ])+'</tr>'
+  color=["#ffe6ff","#FAFAD2"]
+  td=[
+      f'<tr style="background-color:{color[wo_details%2]};">'+f'<td>{wo_details+1}</td>'+
+      ''.join([ f'<td>{wo[wo_details][list(wo[wo_details].keys())[i]]}</td>' for i in range(len(wo[wo_details])) ])+
+      f'<td>{statusindicator(status[wo_details])}</td><td>{button()}</td></tr><tr style="background-color:{color[wo_details%2]};"><td colspan=11>{progress(wo[wo_details]["produced_qty"],wo[wo_details]["in_process"],wo[wo_details]["not_started"])}</td></tr>' for wo_details in range(len(wo))]
+  
+  return head+style+jobcardinfohtml(salesorder)+'<table>'+html+''.join(td)+'</table>'
