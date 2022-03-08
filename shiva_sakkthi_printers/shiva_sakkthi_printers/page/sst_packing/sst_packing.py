d51c363cb614a2d6ba53ebc919ca9baf56b6c10a
hiraniya <hiraniya@thirvusoft.in>
2022-03-08 11:52:55 +0530

    Packing page READY

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_packing/sst_packing.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/sst_packing/sst_packing.py
@@ -38,28 +38,73 @@ def jobcarddetails(salesorder):
       'stock':0 if(get_data(item_code=frappe.get_all("Item",{"item_name":i.item_name})[0].name , warehouse="Finished Goods - SST")==[]) else int(get_data(item_code=frappe.get_all("Item",{"item_name":i.item_name})[0].name , warehouse="Finished Goods - SST")[0]['actual_qty']) 
     }  for i in work_order]
     status=[ i.status for i in work_order]
-    return jobcardhtml(work_order_dict,status,salesorder) if(len(work_order_dict)>0) else '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"></head> <center><b> NO JOB CARDS </b></center>'
-    
+    wo_itemcode=[frappe.get_all("Item",{'item_name':i.item_name})[0].name for i in work_order]
+    wo_warehouse=[i.fg_warehouse for i in work_order]
+    return jobcardhtml(wo_warehouse,wo_itemcode,work_order_dict,status,salesorder) if(len(work_order_dict)>0) else '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"></head> <center><b> NO JOB CARDS </b></center>'
+
+
+@frappe.whitelist()
+def createstockentry(item_code,qty,warehouse):
+  doc=frappe.new_doc("Stock Entry")
+  doc.update({
+    'doctype':'Stock Entry',
+    'stock_entry_type':'Material Transfer',
+    'from_warehouse':warehouse,
+  })
+  stock_list=[]
+  stock_list.append({
+      "item_code" : item_code,
+      "t_warehouse":'Dispatch - SST',
+      'qty':qty
+  })
+  stock_list.append({
+      "item_code" : item_code,
+      "t_warehouse":'Finished Goods - SST',
+      'qty':qty
+  })
+  stock_list.append({
+      "item_code" : item_code,
+      "t_warehouse":'Damage Goods - SST',
+      'qty':qty
+  })
+  doc.set('items', stock_list)
+  doc.save()
+  return doc.name
+
+
+
 def script():
-  scr= f'''
+  scr= '''
     <script>
-      function Completed(){{
-        frappe.set_route("stock-entry","new-stock-entry-1");
-        location.reload();
-      }}
+      function Completed(item_code,qty,warehouse){
+        console.log(item_code);
+        frappe.call(
+						{
+							method:"shiva_sakkthi_printers.shiva_sakkthi_printers.page.sst_packing.sst_packing.createstockentry",
+							args:{
+								"item_code":item_code,
+                "qty":qty,
+                "warehouse":warehouse
+							},
+							callback:function(r){
+                frappe.set_route("stock-entry",r.message);
+							}
+            })
+       
+      }
     </script>
   '''
   return scr
 
 
-def button():
-  return '''
-    <button onclick="Completed()" class="Completed" type="button" style="background-color:#80ffaa; margin-bottom:4px;"> Completed </button>
+def button(item_code,wo,warehouse):
+  return f'''
+    <button onclick="Completed('{item_code}','{wo['qty']}','{warehouse}')" class="Completed" type="button" style="background-color:#80ffaa; margin-bottom:4px;"> Completed </button>
   '''
 
 
 
-def jobcardhtml(wo,status,salesorder):
+def jobcardhtml(wo_warehouse,wo_itemcode,wo,status,salesorder):
   head='<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"></head>'
   style='''<style> 
               tr,th,td {
@@ -106,7 +151,7 @@ def jobcardhtml(wo,status,salesorder):
   td=[
       f'<tr style="background-color:{color[wo_details%2]};">'+f'<td>{wo_details+1}</td>'+
       ''.join([ f'<td>{wo[wo_details][list(wo[wo_details].keys())[i]]}</td>' for i in range(len(wo[wo_details])) ])+
-      f'<td>{button()}</td></tr>'
+      f'<td>{button(wo_itemcode[wo_details],wo[wo_details],wo_warehouse[wo_details])}</td></tr>'
       for wo_details in range(len(wo))
      ]
   return head+style+script()+jobcardinfohtml(salesorder)+'<table>'+html+''.join(td)+'</table>'
