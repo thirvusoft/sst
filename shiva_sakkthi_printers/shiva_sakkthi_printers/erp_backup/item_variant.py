6a7e8fdea31afc5d44d1865f25f5a789a1b1e20c
Hiraniya S <95607683+hiraniya@users.noreply.github.com>
2022-03-09 12:25:11 +0530

    Update item_variant.py

--- shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/item_variant.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/item_variant.py
@@ -1,1 +1,16 @@
+Core - Line Number:330 & 331
+  Version - 13.19.0
+  Core Path: erpnext/controllers/item_variant.py
+   Existing Code:
+    ```
+    variant.item_code = "{0}-{1}".format(template_item_code, "-".join(abbreviations))
+		variant.item_name = "{0}-{1}".format(template_item_name, "-".join(abbreviations))
 
+    
+    ```
+   Code to be changed:
+    ```
+    	variant.item_code = "{0}/{1}".format(template_item_code, "/".join(abbreviations))
+	  	variant.item_name = "{0}/{1}".format(template_item_name, "/".join(abbreviations))
+    
+    ```
