3e000dfb2ed7a6bcb1296d59bedd02f3618d4c84
Hiraniya S <95607683+hiraniya@users.noreply.github.com>
2022-03-09 13:28:20 +0530

    Create work_order.py

---
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/work_order.py
@@ -0,0 +1,24 @@
+Core - Line Number:5+
+  Version - 13.19.0
+  Core Path: erpnext/manufacturing/doctype/work_order/work_order.js
+   Existing Code:
+    ``` 
+    ```
+   Code to be changed:
+    ```
+     import math
+    
+    ```
+    
+Core - Line Number:283,284
+   Existing Code:
+    ``` 
+    	d.required_qty = item_dict.get(d.item_code).get("qty")
+    
+    ```
+   Code to be changed:
+    ```
+      print('\n'*5,d.available_qty_at_wip_warehouse,item_dict.get(d.item_code).get("qty"))
+			d.required_qty = math.ceil(item_dict.get(d.item_code).get("qty"))
+    
+    ```
