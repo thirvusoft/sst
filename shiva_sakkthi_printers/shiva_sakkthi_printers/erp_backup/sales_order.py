3253bced76fad15883ac40323e392e079ac3922f
Hiraniya S <95607683+hiraniya@users.noreply.github.com>
2022-03-09 13:47:27 +0530

    Update sales_order.py

--- shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/sales_order.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/sales_order.py
@@ -1,4 +1,4 @@
-Core - Line Number:19 -25
+Core - Line Number:11+ 
   Version - 13
   Core Path:
    Existing Code:
@@ -6,5 +6,42 @@ Core - Line Number:19 -25
     ```
    Code to be changed:
     ```
+    from erpnext.stock.dashboard.item_dashboard import get_data
+    
     ```
+    
+Core - Line Number:(410+, 411+ )
+  Existing Code:
+    ```
+    ```
+   Code to be changed:
+    ```
+    	       excess_percentage=0,
+							stock= 0 if(get_data(item_code=i.item_code , warehouse='Finished Goods - SST')==[]) else get_data(item_code=i.item_code , warehouse='Finished Goods - SST')[0]['actual_qty'],
+    
+    ```
+    
+ Core - Line Number:(423+, 424+ )
+   Existing Code:
+    ```
+    ```
+   Code to be changed:
+    ```
+    	        excess_percentage=0,
+							stock= 0 if(get_data(item_code=i.item_code , warehouse='Finished Goods - SST')==[]) else get_data(item_code=i.item_code , warehouse='Finished Goods - SST')[0]['actual_qty'],
+    
+    ```
+    
+    Core - Line Number:1025+
+       Existing Code:
+    ```
+       qty=i['pending_qty'],
+    
+    ```
+      Code to be changed:
+    ```
+    	qty=(((i['pending_qty']-i['stock'])*i['excess_percentage'])/100)+i['pending_qty'],
+    	        
+    ```
+    
   
