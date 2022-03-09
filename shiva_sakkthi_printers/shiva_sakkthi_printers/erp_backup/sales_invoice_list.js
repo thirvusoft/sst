6b9ff794705b64c2fd0999983ae14aa67fb4329d
Hiraniya S <95607683+hiraniya@users.noreply.github.com>
2022-03-09 11:59:07 +0530

    Update sales_invoice_list.js

--- shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/sales_invoice_list.js
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/sales_invoice_list.js
@@ -1,1 +1,13 @@
-
+Core - Line Number:11
+  Version - 13.19.0
+  Core Path: erpnext/accounts/doctype/sales_invoice/sales_invoice_list.js
+   Existing Code:
+    ```
+    "Unpaid": "orange",
+    
+    ```
+   Code to be changed:
+    ```
+    	"Payment pending": "yellow",
+    
+    ```
