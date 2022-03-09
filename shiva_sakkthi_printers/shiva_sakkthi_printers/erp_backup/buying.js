e4881111ec3fd5da80af6aba42ed03e96ba0d14f
Hiraniya S <95607683+hiraniya@users.noreply.github.com>
2022-03-09 13:36:40 +0530

    Update buying.js

--- shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/buying.js
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/buying.js
@@ -1,1 +1,42 @@
-
+Core - Line Number:84 to 101
+  Version - 13.19.0
+  Core Path: erpnext/public/js/controllers/buying.js
+   Existing Code:
+    ```
+    if (me.frm.doc.is_subcontracted == "Yes") {
+				return{
+					query: "erpnext.controllers.queries.item_query",
+					filters:{ 'supplier': me.frm.doc.supplier, 'is_sub_contracted_item': 1 }
+				}
+			}
+			else {
+				return{
+					query: "erpnext.controllers.queries.item_query",
+					filters: { 'supplier': me.frm.doc.supplier, 'is_purchase_item': 1 }
+				}
+			}
+		});
+    
+    ```
+   Code to be changed:
+    ```
+    		// 	if (me.frm.doc.is_subcontracted == "Yes") {
+				return{
+		// 			query: "erpnext.controllers.queries.item_query",
+		//	filters:{ 'supplier': me.frm.doc.supplier, 'is_sub_contracted_item': 1 }
+		     filters: {
+		       	'item_group' : 'By-Product'
+		     }
+				}
+		 });   
+		// 		}
+		// 	}
+		// 	else {
+		// 		return{
+		// 			query: "erpnext.controllers.queries.item_query",
+		// 			filters: { 'supplier': me.frm.doc.supplier, 'is_purchase_item': 1 }
+		// 		}
+		// 	}
+		// });
+    
+    ```
