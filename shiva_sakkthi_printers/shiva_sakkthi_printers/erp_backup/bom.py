1befcf95315ad8228f917bcc970b3c50b77d047a
Hiraniya S <95607683+hiraniya@users.noreply.github.com>
2022-03-09 12:32:23 +0530

    Update bom.py

--- shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/bom.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/erp_backup/bom.py
@@ -1,1 +1,14 @@
-
+Core - Line Number:1133+, 1134+
+  Version - 13.19.0
+  Core Path: erpnext/accounts/doctype/sales_invoice/sales_invoice_list.js
+   Existing Code:
+    ```
+   
+    
+    ```
+   Code to be changed:
+    ```
+    	if filters and filters.get('filter_by_item_group'):
+		  query_filters['item_group'] = 'By-Product'
+    
+    ```
