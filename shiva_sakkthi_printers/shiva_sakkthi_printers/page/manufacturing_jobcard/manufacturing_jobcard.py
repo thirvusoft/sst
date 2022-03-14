e237c6b7b830772205188155126c74453e5d45c5
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-03-14 12:25:26 +0530

    Job card page changed items seperated  by design, paper quality, colour

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/manufacturing_jobcard/manufacturing_jobcard.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/manufacturing_jobcard/manufacturing_jobcard.py
@@ -1,7 +1,5 @@
-from re import S
 import frappe
-from erpnext.stock.dashboard.item_dashboard import get_data
-    
+
 @frappe.whitelist()
 def sofilter(status='',customer=''):
     filters={'status':['!=',"Completed"]}
@@ -36,87 +34,90 @@ def jobcarddetails(salesorder):
     work_order_dict=[{
       'workorder':i.item_name,
       'qty':int(i.qty),
-      'stock':0 if(get_data(item_code=frappe.get_all("Item",{"item_name":i.item_name})[0].name , warehouse="Finished Goods - SST")==[]) else int(get_data(item_code=frappe.get_all("Item",{"item_name":i.item_name})[0].name , warehouse="Finished Goods - SST")[0]['actual_qty']) ,
-      'raw_material_qty':int(i.required_items[0].required_qty)  }  for i in work_order]
-    seperated_work_order=workorderfunc(work_order_dict)
-    status=[ i.status for i in work_order]
-    workorder=[i.name for i in work_order]
-    htmlcode=jobcardinfohtml(salesorder)
-    for workorderdict in seperated_work_order:
-        htmlcode+=jobcardhtml(workorderdict,seperated_work_order[workorderdict],status,salesorder,workorder)
-    htmlcode+=description(work_order_dict)
-    return htmlcode if(len(work_order_dict)>0) else '<center><b> NO JOB CARDS </b></center>'
+      'stock':int(i.produced_qty),
+      'production':int(i.qty)-int(i.produced_qty),
+      'raw_material_qty':int(i.required_items[0].required_qty),
+      'raw_material':i.required_items[0].item_name }  for i in work_order]
+    work_order_attr=workorderfunc(work_order_dict)
+    work_order_dict=work_order_sep(work_order_attr)
+    
+        
+    
+    html=jobcardhtml(work_order_dict,salesorder,work_order_attr)
+    return html if(len(work_order_dict)>0) else '<center><b> NO JOB CARDS </b></center>'
+ 
+    
+    
  
  
- 
-def workorderfunc(wo):
-    seperated_work_order={}
-    for i in wo:
-        itemcode=frappe.get_all('Item',{'item_name':i['workorder']})
-        item=frappe.get_doc('Item',itemcode[0].name)
-        for attr in item.attributes:
-            if(attr.attribute=='Colour'):
-                color=attr.attribute_value
-        if(color in seperated_work_order):
-            seperated_work_order[color].append(i)
-        else:
-            seperated_work_order[color]=[i]
-    return seperated_work_order
-        
-        
-        
-        
- 
-def statusindicator(status):
-  color={
-    "In Process": "orange",
-    "Completed": "green",
-    "Not Started": "red",
-    "Stopped": "red",
-    "Closed": "black",
-    "Cancelled": "red"
-  }
-  return f'<span class="indicator-pill whitespace-nowrap {color[status]}"><span style="font-weight:bold;font-size:14px;">{status}</span></span>'
+
+def work_order_sep_attr(wo, attr):
+  workorder={}
+  for i in wo:
+    if(i[attr] in workorder):
+      workorder[i[attr]].append(i)
+    else:
+      workorder[i[attr]]=[i]
+  return workorder
 
 
+
+def work_order_sep_attr2(wo, attr):
+  workorder={}
+  for i in wo:
+    workorder[i]=work_order_sep_attr(wo[i],attr)
+  return workorder
   
-def script():
-  scr= f'''
-    <script>
-      function start(wo){{
-        console.log('hiii',wo);
-        frappe.set_route("work-order",wo);
-        location.reload();
-      }}
-    </script>
-  '''
-  return scr
 
 
-def button(wo):
-  return f'''
-    <button onclick="start('{wo}')" class="start" type="button" style="background-color:#4da6ff; margin-bottom:4px;"> Start </button>
-  '''
+def work_order_sep_attr3(wo, attr):
+  workorder={}
+  for i in wo:
+    if(i[attr] in workorder):
+      workorder[i[attr]].append(i)
+    else:
+      workorder[i[attr]]=[i]
+  return workorder
 
 
 
-def progress(produced,inprocess,not_started):
-  bar=f'''
-    <div class="progress">
-      <div class="progress-bar bg-success" style="width:{produced}%">
-        <b>Produced {produced}</b>
-      </div>
-      <div class="progress-bar bg-warning" style="width:{inprocess}%;font-color:white">
-        <b>Inprocess {inprocess}</b>
-      </div>
-      <div class="progress-bar bg-gray" style="width:{not_started}%">
-        <b>Not Started {not_started}</b>
-      </div>
-    </div> 
-  '''
-  return bar
+def work_order_sep_color(wo):
+  workorder={}
+  for i in wo:
+    worder={}
+    for j in wo[i]:
+      worder[j]=work_order_sep_attr3(wo[i][j],'colour')
+    workorder[i]=worder
+  return workorder
+  
 
 
+def work_order_sep(wo):
+  wo=work_order_sep_attr(wo, 'design')
+  wo=work_order_sep_attr2(wo, 'raw_material')
+  wo=work_order_sep_color(wo)
+  return wo
+  
+
+def workorderfunc(wo):
+    workorder=[]
+    for i in wo:
+      item=i['workorder']
+      docs=frappe.get_all('Item',{'item_name':item})
+      doc=frappe.get_doc('Item',docs[0].name)
+      attr={}
+      for j in doc.attributes:
+        attr[j.attribute]=j.attribute_value
+      i['size']=attr['Size']
+      i['colour']=attr['Colour']
+      i['design']=doc.variant_of
+      workorder.append(i)
+    return workorder
+     
+        
+        
+        
+ 
 
 def jobcardinfohtml(salesorder):
   so=frappe.get_doc('Sales Order',salesorder)
@@ -124,7 +125,7 @@ def jobcardinfohtml(salesorder):
     <div class='div'>
       <div class='jobcardinfo'>
         Customer Name : {so.customer}<br>
-        Delivery Date : {so.delivery_date}<br><br>
+        Delivery Date : {'-'.join(str(so.delivery_date).split('-')[::-1])}<br><br>
       </div>
       <div class='jobcardinfo'>
         Job Card NO : {so.name}<br>
@@ -136,9 +137,7 @@ def jobcardinfohtml(salesorder):
 
 
 def description(wo):
-  print('\n'*5,wo)
   total_papers=sum([i['raw_material_qty'] for i in wo])
-  print(total_papers,'\n'*3)
   return f'''
     <div class='div'>
     Total No of Papers: {total_papers}
@@ -153,16 +152,40 @@ def html_style():
         padding: 6px;
       }
       .table1{
-        width: 100%;
+        width:100%;
+        text-align:center;
         margin-bottom: 5px;
       }
       .heading{
         font-weight: bold;
-        border: 1px solid black;
+        font-size:17px;
         width: 10%;
       }
       .data{
+        font-weight: 600;
+        font-size:16px;
+      }
+      td {
         border: 1px solid black;
+      }
+      .jobcardinfo{
+        float:left;
+        width:50%;
+      }
+      .div{
+        background-color:#33307c;
+        color:white;
+        font-weight:bold;
+        border-radius:10px;
+        height:100px;
+        width:100%;
+        margin-bottom:5px;
+        padding:20px;
+        font-size:17px;
+        line-height:2;
+      }
+      b{
+        font-size:19px;
       }
     </style>
   '''
@@ -172,27 +195,54 @@ def html_style():
 def no_of_paper(wo):
   return f'''
       <b>
-        No of Papers: {sum([i["raw_material_qty"] for i in wo])}
+        No of Papers: {sum([i["noofpaper"] for i in wo])}
       </b>'''
 
 
-
-def jobcardhtml(color,wo,status,salesorder,workorder):
-  wo_list=list(zip(*[list(i.values()) for i in wo]))
-  html_code=[]
-  colorhtml=f'''
-    <b> Colour: {color}</b>
+def htmlfortable(color,wo):
+  workorder=[{'size':i['size'],'qty':i['qty'],'stock':i['stock'],
+      'production':i['production'],'noofpaper':i['raw_material_qty']} for i in wo]
+  wo_list=list(zip(*[list(i.values()) for i in workorder]))
+  html=f'''
+    <h4>Colour : {color}<br></h4>
   '''
   trs=['<tr><td class="heading">Size</td>',
-       '<tr><td class="heading">Qty</td>',
-       '<tr><td class="heading">Stock</td>',
-       '<tr><td class="heading">No of Paper</td>']  
+      '<tr><td class="heading">Qty</td>',
+      '<tr><td class="heading">Stock</td>',
+      '<tr><td class="heading">Production</td>',
+      '<tr><td class="heading">No of Paper</td>'] 
   table='<table class="table1">'
   for i,j in zip(trs,wo_list):
     table+=i+''.join( [f'<td class="data">{x}</td>' for x in j] )+'</tr>'
-  html_code.append(html_style())
-  html_code.append(colorhtml)
-  html_code.append(table+'</table>'+no_of_paper(wo)+'<BR><br><br>')
-  return ''.join(html_code)
+  html+=html_style()
+  html+=table+'</table>'+no_of_paper(workorder)+'<BR><br><br>'
+  return html
+
+
+def htmlforpaper(paper,wo):
+  html=f'''
+    <h3>Paper : {paper}<br></h3>
+  '''
+  for color in wo:
+    html+=htmlfortable(color,wo[color])
+  return html
+  
+
+def htmlfordesign(design,wo):
+  html=f'''
+    <h2>Design : {design}<br></h2>
+  '''
+  for paper in wo:
+    html+=htmlforpaper(paper,wo[paper])
+  return html
+
+
+def jobcardhtml(wo,so,unsepwo):
+  htmlcode=jobcardinfohtml(so)
+  print('\n'*10)
+  for design in wo:
+    htmlcode+=htmlfordesign(design,wo[design])+'<br><br>  '
+  return htmlcode+description(unsepwo)
+
     
     
