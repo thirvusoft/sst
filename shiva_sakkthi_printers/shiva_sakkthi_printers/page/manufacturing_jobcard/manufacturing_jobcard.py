54c2a9f62959ed8a80ff0c6705df745324ee3d6a
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-03-15 09:09:00 +0530

    job card change style modified

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/manufacturing_jobcard/manufacturing_jobcard.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/manufacturing_jobcard/manufacturing_jobcard.py
@@ -1,4 +1,5 @@
 import frappe
+import math
 
 @frappe.whitelist()
 def sofilter(status='',customer=''):
@@ -37,7 +38,9 @@ def jobcarddetails(salesorder):
       'stock':int(i.produced_qty),
       'production':int(i.qty)-int(i.produced_qty),
       'raw_material_qty':int(i.required_items[0].required_qty),
-      'raw_material':i.required_items[0].item_name }  for i in work_order]
+      'raw_material':i.required_items[0].item_name,
+      'bomquantity':math.ceil(( (i.qty or 0)-(i.produced_qty or 0))/(frappe.db.get_value("BOM", {"item_name": i.item_name, 'is_default':1},"quantity") or 0))
+}  for i in work_order]
     work_order_attr=workorderfunc(work_order_dict)
     work_order_dict=work_order_sep(work_order_attr)
     
@@ -119,51 +122,54 @@ def workorderfunc(wo):
         
  
 
-def jobcardinfohtml(salesorder):
+def jobcardinfohtml(salesorder,wo):
+  total_papers=sum([i['bomquantity'] for i in wo])
   so=frappe.get_doc('Sales Order',salesorder)
   infohtml=f'''
     <div class='div'>
       <div class='jobcardinfo'>
         Customer Name : {so.customer}<br>
-        Delivery Date : {'-'.join(str(so.delivery_date).split('-')[::-1])}<br><br>
+        Delivery Date : {'-'.join(str(so.delivery_date).split('-')[::-1])}<br>
+        TOTAL PAPERS: {total_papers}<br><br>
       </div>
       <div class='jobcardinfo'>
         Job Card NO : {so.name}<br>
-        PO No : {so.po_no}<br><br>
+        PO No : {so.po_no or '-'}<br><br>
       </div>
     </div>
   '''
   return infohtml
 
 
-def description(wo):
-  total_papers=sum([i['raw_material_qty'] for i in wo])
-  return f'''
-    <div class='div'>
-    Total No of Papers: {total_papers}
-    </div>
-  '''
 
 
 def html_style():
   style='''
     <style>
+      .designdiv{
+        border: 1px solid black;
+        padding: 10px;
+        border-radius: 5px;
+        margin-top: 15  px;
+      }
       td{
         padding: 6px;
       }
       .table1{
-        width:100%;
-        text-align:center;
         margin-bottom: 5px;
+        margin-left: auto; 
+        margin-right: auto;
       }
       .heading{
         font-weight: bold;
         font-size:17px;
-        width: 10%;
+        width:160px;
+        background-color: #ccccff;
       }
       .data{
-        font-weight: 600;
         font-size:16px;
+        text-align:center;
+        width:160px;
       }
       td {
         border: 1px solid black;
@@ -177,7 +183,7 @@ def html_style():
         color:white;
         font-weight:bold;
         border-radius:10px;
-        height:100px;
+        height:140px;
         width:100%;
         margin-bottom:5px;
         padding:20px;
@@ -185,7 +191,25 @@ def html_style():
         line-height:2;
       }
       b{
-        font-size:19px;
+        font-size:25px;
+      }
+      .image{
+        height: 150px;
+        width: 150px;
+        float: right;
+        border: 1px solid black;
+      }
+      .nop{
+        text-align: center;
+        background-color:#33307c;
+        color:white;
+        border-radius: 5px;
+        width: 300px;
+        font-weight: bold;
+        font-size: 25px;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, 10%);
       }
     </style>
   '''
@@ -194,55 +218,57 @@ def html_style():
 
 def no_of_paper(wo):
   return f'''
-      <b>
-        No of Papers: {sum([i["noofpaper"] for i in wo])}
-      </b>'''
+      <div class='nop'>
+          No of Papers: {sum([i["noofpaper"] for i in wo])}
+      </div>'''
 
 
-def htmlfortable(color,wo):
+def htmlfortable(paper,color,wo):
   workorder=[{'size':i['size'],'qty':i['qty'],'stock':i['stock'],
-      'production':i['production'],'noofpaper':i['raw_material_qty']} for i in wo]
+      'production':i['production'],'noofpaper':i['bomquantity']} for i in wo]
   wo_list=list(zip(*[list(i.values()) for i in workorder]))
   html=f'''
-    <h4>Colour : {color}<br></h4>
+    <h3>Paper : {paper}<br></h3>
+    <h4>Colour : {color}<br><br><br></h4>
   '''
-  trs=['<tr><td class="heading">Size</td>',
+  trs=['<tr style="font-weight:bold;background-color: #ccccff;"><td class="heading">Size</td>',
       '<tr><td class="heading">Qty</td>',
       '<tr><td class="heading">Stock</td>',
-      '<tr><td class="heading">Production</td>',
-      '<tr><td class="heading">No of Paper</td>'] 
+      '<tr style="font-weight:bold;"><td class="heading">Production</td>',
+      '<tr style="font-weight:bold"><td class="heading">No of Paper</td>'] 
   table='<table class="table1">'
   for i,j in zip(trs,wo_list):
     table+=i+''.join( [f'<td class="data">{x}</td>' for x in j] )+'</tr>'
   html+=html_style()
-  html+=table+'</table>'+no_of_paper(workorder)+'<BR><br><br>'
+  html+=table+'</table>'+no_of_paper(workorder)+'<BR><br><hr><br>'
   return html
 
 
 def htmlforpaper(paper,wo):
-  html=f'''
-    <h3>Paper : {paper}<br></h3>
-  '''
+  html=''
   for color in wo:
-    html+=htmlfortable(color,wo[color])
+    html+=htmlfortable(paper,color,wo[color])
   return html
   
 
 def htmlfordesign(design,wo):
+  url=frappe.get_value('Item',design,'image')
   html=f'''
+    <div class="designdiv">
+    <img class="image" src="{url}"></img>
     <h2>Design : {design}<br></h2>
   '''
   for paper in wo:
     html+=htmlforpaper(paper,wo[paper])
-  return html
+  return html+'</div>'
 
 
 def jobcardhtml(wo,so,unsepwo):
-  htmlcode=jobcardinfohtml(so)
+  htmlcode=jobcardinfohtml(so,unsepwo)
   print('\n'*10)
   for design in wo:
     htmlcode+=htmlfordesign(design,wo[design])+'<br><br>  '
-  return htmlcode+description(unsepwo)
+  return htmlcode
 
     
     
