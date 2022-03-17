42cdc4d092d69bd078b20e76e5e6153a7db3ecf2
hiraniya <hiraniya@thirvusoft.in>
2022-03-17 15:15:54 +0530

    Original,Duplicate,Triplicate

--- shiva_sakkthi_printers/custom/sales_order_list.js
+++ shiva_sakkthi_printers/custom/sales_order_list.js
@@ -1,4 +1,4 @@
-frappe.listview_settings['Sales Order'] = {
+/*frappe.listview_settings['Sales Order'] = {
 	add_fields: ["base_grand_total", "customer_name", "currency", "delivery_date",
 		"per_delivered", "per_billed", "status", "order_type", "name", "skip_delivery_note"],
 	get_indicator: function (doc) {
@@ -50,3 +50,4 @@ frappe.listview_settings['Sales Order'] = {
 
 	}
 };
+*/
