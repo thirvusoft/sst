5bda1efc1101fb62e6d1e8dd749e25fae1c96d13
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-03-28 11:11:58 +0530

    tax fetching code updated

--- shiva_sakkthi_printers/custom/sales_invoice.js
+++ shiva_sakkthi_printers/custom/sales_invoice.js
@@ -1,7 +1,9 @@
 frappe.ui.form.on('Sales Invoice',{
 refresh:function(frm){
-    var taxes_and_charges= cur_frm.doc.taxes_and_charges;
-    cur_frm.set_value('taxes_and_charges','');
-    cur_frm.set_value('taxes_and_charges',taxes_and_charges)
+    if(!cur_frm.doc.taxes){
+        var taxes_and_charges= cur_frm.doc.taxes_and_charges;
+        cur_frm.set_value('taxes_and_charges','');
+        cur_frm.set_value('taxes_and_charges',taxes_and_charges)
+    }
 }
 })
