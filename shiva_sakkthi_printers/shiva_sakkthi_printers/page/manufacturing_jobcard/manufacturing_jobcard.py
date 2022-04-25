3c59e2bd4115252a47115b73201940e7b4c9e414
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2022-04-25 17:27:23 +0530

    open a bigger image while clicking on it

--- shiva_sakkthi_printers/shiva_sakkthi_printers/page/manufacturing_jobcard/manufacturing_jobcard.py
+++ shiva_sakkthi_printers/shiva_sakkthi_printers/page/manufacturing_jobcard/manufacturing_jobcard.py
@@ -173,6 +173,23 @@ def js_script():
         }
       })
     }
+    
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   </script>
   '''
   return script
@@ -317,6 +334,41 @@ def html_style():
         font-size:30px;
         color:#e6b800;
       }
+      .modal {
+        display: none;
+        position: fixed; 
+        z-index: 1; 
+        padding-top: 130px;
+        left: 0;
+        top: 0;
+        width: 100%; 
+        height: 100%;
+        background-color: rgb(0,0,0); 
+        background-color: rgba(0,0,0,0.4); 
+      }
+
+      .modal-content {
+        background-color: #fefefe;
+        margin: auto;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 60%;
+        height: 80%;
+      }
+
+      .close {
+        color: #aaaaaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+      }
+
+      .close:hover,
+      .close:focus {
+        color: #000;
+        text-decoration: none;
+        cursor: pointer;
+      }
     </style>
   '''
   return style
@@ -361,8 +413,18 @@ def htmlfordesign(design,wo):
   url=frappe.get_value('Item',design,'image')
   html=f'''
     <div class="designdiv">
-    <img class="image" src="{url}"></img>
+    <div class="image">
+    <button id="myBtn"><img src="{url}"></img></button>
+    </div>
     <h2>Design : {design}<br></h2>
+    <div id="myModal" class="modal">
+
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <img style="height:100%;width:100%;" src="{url}"></img>
+    </div>
+
+</div>
   '''
   for paper in wo:
     html+=htmlforpaper(paper,wo[paper])
